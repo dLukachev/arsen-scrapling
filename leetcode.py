3787f95b4c87424455b5473802b63285fec3140f
Danil <ddrxg@MacBook-Pro-Danil.local>
2026-04-04 15:31:38 +0300

    добавлены задачки литкода

---
+++ leetcode.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:  
+        a = 'aBCDEFGHIJKLMNOPQSTUVWXYZ'.lower()
+        detect  = [i for i in a]
+        for b in a: 
+            if b in sentence: 
+                
+
+
+        
+# for sentence in a:
+# a = 'abc....' -> sentence = a, b, c, ...
+
+# for a in sentence
+# sentence = "leetcode" -> a = l, e, e, t....
+
+# if "leetcode" in l: -> else
+# else = return False
+
+
+
+        
+
+            
